d7d737e2b03ec3408803541736c4aeff1046441b
Samantha-lu <1195872272@qq.com>
2018-09-11 22:02:46 +0800

    wrote a readme file

---
+++ homework01.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class homework01
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            int n1 = 0,n2=0;
+            Console.Write("输入一个整型乘数：");
+            s = Console.ReadLine();
+            n1 = Int32.Parse(s);
+            Console.Write("输入另一个整型乘数：");
+            s = Console.ReadLine();
+            n2 = Int32.Parse(s);
+            Console.Write("这两个数的乘积是："+(n1*n2));
+
+        }
+    }
+}
+
+
